6d07d48376bb40ab3e82ea62e795543a90a6dfea
wojtek <zaskier@gmail.com>
2023-11-07 19:29:05 +0100

    add default and custom 404

---
+++ app/not-found.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <main className="text-center">
+      <h2 className="text-3x1">There was an problem.</h2>
+      <p>We could not found page you were looking for</p>
+      <p>
+        <Link href="/">Dashboard</Link>
+      </p>
+    </main>
+  );
+}
