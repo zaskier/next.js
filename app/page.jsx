4786f5ff391696e7e0955cebdade5f118c464beb
wojtek <zaskier@gmail.com>
2023-11-07 13:35:15 +0100

    init next.js with tailwind

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <h2>Dashboards</h2>
+    </main>
+  );
+}
---
+++ app/tickets/page.jsx
@@ -0,0 +1,7 @@
+export default function Tickets() {
+  return (
+    <div>
+      <h2>Tickets</h2>
+    </div>
+  );
+}
