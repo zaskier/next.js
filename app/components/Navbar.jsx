24e67c28662981bd132ce5d3d19e478e78f9135a
wojtek <zaskier@gmail.com>
2023-11-07 14:34:56 +0100

    added content

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Logo from 'public/ev_corp_logo.png';
 
 export default function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt=" EvCorp helpdesk logo" width={70} quality={70} placeholder="blur" />{' '}
       <h1>EvCorp helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
