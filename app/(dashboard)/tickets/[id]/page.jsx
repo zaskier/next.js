a22326e685c7af2c54c9b46f01fed9b5099469a5
wojtek <zaskier@gmail.com>
2023-11-09 15:14:08 +0100

    generate dynamic metadana

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -2,6 +2,16 @@ import { notFound } from 'next/navigation';
 
 export const dynamicParams = true;
 
+export async function generateMetadata({ params }) {
+  const id = params.id;
+
+  const res = await fetch(`http://localhost:4000/tickets/${id}`);
+  const ticket = await res.json();
+  return {
+    title: `E-Corp Helpdesk | ${ticket.title}`
+  };
+}
+
 export async function generateStaticParams() {
   const res = await fetch(`http://localhost:4000/tickets`);
   const tickets = await res.json();
--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -2,6 +2,10 @@ import { Suspense } from 'react';
 import TicketList from './TicketList';
 import Loading from '../loading';
 
+export const metadata = {
+  title: 'E-Corp Helpdesk | Tickets'
+};
+
 export default function Tickets() {
   return (
     <main>
