2f3d4b236e65a84de876b000b2ed691e2eedf8ef
wojtek <zaskier@gmail.com>
2023-11-09 19:49:59 +0100

    add rest api route

---
+++ app/api/tickets/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
+export async function GET() {
+  const res = await fetch(`http://localhost:4000/tickets`);
+  const tickets = await res.json();
+
+  return NextResponse.json(tickets, {
+    status: 200
+  });
+}
