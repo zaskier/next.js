852834cb3d6c341d80c32b1247818fd0bd72c820
wojtek <zaskier@gmail.com>
2023-11-12 22:44:15 +0100

    implement delete ticket

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -1,14 +1,27 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 import { TiDelete } from 'react-icons/ti';
 
 export default function DeleteButton({ id }) {
   const [isLoading, setIsLoading] = useState(false);
-
+  const router = useRouter();
   const handleClick = async () => {
     setIsLoading(true);
-    console.log('deleting id - ', id);
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+      method: 'DELETE'
+    });
+    const json = await res.json();
+
+    if (json.error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+    if (!json.error) {
+      router.refresh();
+      router.push('/tickets');
+    }
   };
 
   return (
